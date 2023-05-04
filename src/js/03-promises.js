c90e3a712bcad223444c5dbe357a6f4009990eb8
OlegatorIT <podlesnyoleg93@gmail.com>
2023-05-04 16:43:03 +0200

    task-3 is done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+let counter = 1;
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  repeatPromis();
+});
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const step = Number(formEl.elements.step.value);
+  delay = Number(formEl.elements.delay.value) + counter * step;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+    counter++;
+  });
+}
+
+function repeatPromis() {
+  const amount = Number(formEl.elements.amount.value);
+
+  const interval_ID = setInterval(() => {
+    if (counter <= amount) {
+      createPromise(counter)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+    } else {
+      clearInterval(interval_ID);
+      counter = 1;
+    }
+  }, 0);
 }
